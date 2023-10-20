364b56e211ceae3ace3f04b651f305e61833578a
dr3amcoder <jannalimpin@hotmail.co.uk>
2023-10-20 17:15:46 +0100

    Popular recipe page done

--- frontend/src/components/Home/home.js
+++ frontend/src/components/Home/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "./home.css"
 import "../generator/Generator"
+import "../popular_recipes/PopularRecipes"
 
 
 
@@ -9,15 +10,17 @@ function Home(){
     return (
         <>
             {/* <Navbar currentPage="home" />  */}
+            <h1 className = "main-title">Flavor Finders</h1>
+            <h4 className = "slogan">Season the Day!</h4>
             <div className = "first-button">
-                <h1 className = "main-title">Flavor Finders</h1>
-                <h4 className = "slogan">Season the Day!</h4>
-                <button className = "popular-recipes">Don't know where to start? Click here for popular recipes!</button>
-            <div className = "second-button">
-                <Link to="/generator">
-                    <button className = "generate-recipes">Click here to generate recipes!</button>
+                <Link to="/popular_recipes">
+                    <button className = "popular-recipes">Don't know where to start? Click here for popular recipes!</button>
                 </Link>
             </div>
+            <div className = "second-button">
+                <Link to="/generator">
+                    <button className ="generate-recipes">Click here to generate recipes!</button>
+                </Link>
             </div>
         </>
     )
