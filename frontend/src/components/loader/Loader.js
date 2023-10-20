71138d03edbe8abf5972f9145bfb2822f15d9e97
dr3amcoder <jannalimpin@hotmail.co.uk>
2023-10-20 10:31:37 +0100

    Linked generator  button in the homepage

--- frontend/src/components/loader/Loader.js
+++ frontend/src/components/loader/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as LoaderIcon } from "/Users/alexanderwilson/Projects/flavor_finder_final_project/FlavourFinders/frontend/src/assets/Magnify-1s-200px.svg";
+import { ReactComponent as LoaderIcon } from "/Users/jannalimpin/RecipeGenerator/FlavourFinders/frontend/src/assets/Magnify-1s-200px.svg";
 
 const Loader = () => {
     return (
