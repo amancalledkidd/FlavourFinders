854d01f29477f953414810ffcbcde5b010ce4cea
Khalifa Fadel <fffadel19982@gmail.com>
2023-10-23 13:06:25 +0100

    signup_login_auth

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import lock from "../images/password.jpg";
 import user from "../images/user.jpg";
 import envelope from "../images/envelope.jpg";
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,28 +10,42 @@ const SignUpForm = ({ navigate }) => {
   const [name, setName] = useState()
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    fetch( '/users', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name: name, email: email, password: password })
-    })
-      .then(response => {
-        if(response.status === 201) {
-          navigate('/login')
-        } else {
-          navigate('/signup')
-        }
+    if(!passwordValidating(password)){
+      setErrorMessage("Password not valid. Password must include an uppercase and lowercase character, a special character (@$!%*?&) and be a minimum of 8 characters")
+    }else if(password !== confirmPassword){
+      setErrorMessage("Passwords do not match")
+    }else {
+      fetch( '/users', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: name, email: email, password: password })
       })
+        .then(response => {
+          if(response.status === 201) {
+            navigate('/login')
+          } else {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          setErrorMessage(data.message);
+        });
+    }
   }
   const handleNameChange = (event)=>{
     setName(event.target.value)
   }
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
   const handleEmailChange = (event) => {
     setEmail(event.target.value)
   }
@@ -39,6 +54,11 @@ const SignUpForm = ({ navigate }) => {
     setPassword(event.target.value)
   }
 
+  const passwordValidating = (password) => {
+    const regex =
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(password);
+  };
 
 
     return (
@@ -73,6 +93,7 @@ const SignUpForm = ({ navigate }) => {
                   type="email"
                   placeholder="Enter Email"
                   autoComplete="off"
+                  value={email}
                   name="email"
                   onChange={handleEmailChange}
                 />
@@ -87,16 +108,31 @@ const SignUpForm = ({ navigate }) => {
                   type="password"
                   placeholder="Enter Password"
                   name="password"
+                  value={password}
                   onChange={handlePasswordChange}
                 />
               </div>
+              <div className="input_fields">
+              <img src={lock} className="image_icons" alt="Girl in a jacket" width="24" height="24"/>
+                <label id="password_title" htmlFor="password">
+                  <strong>Confrim Password</strong>
+                </label>
+                <input
+                  id="password_input"
+                  type="password"
+                  placeholder="Confrim Password"
+                  name="password"
+                  value={confirmPassword}
+                  onChange={handleConfirmPasswordChange}
+                />
+              </div>
+              <p className="error">{errorMessage}</p>
               <button type="submit" className="register_button">
                 Register
               </button>
             </form>
             <div>
-              <p className="reminder_msg">Already Have an Account?</p>
-              <a className= "link" href="/login">Login</a>
+            <Link to="/login">Click here to go to the login page</Link>
             </div>
                 
             
