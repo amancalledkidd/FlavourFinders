4efe3cb4a8cfbc638e5559747cfe37747ac8116a
dr3amcoder <jannalimpin@hotmail.co.uk>
2023-10-23 16:31:36 +0100

    Page styling done - added spacing for instructions

--- frontend/src/components/popular_recipes/PopularRecipes.js
+++ frontend/src/components/popular_recipes/PopularRecipes.js
@@ -20,6 +20,7 @@ const PopularRecipe = () => {
                     <h3 className='dish-name'>Pasta Aglio e Olio</h3>
                     <img className='dish-img' src={pasta} alt='Pasta Aglio e Olio'/>
                     <p>Cooking Time: 15 minutes</p>
+                    <br />
                     <p>Ingredients</p>
                         <ul>
                             <li>8 oz (225g) spaghetti or any pasta of your choice</li>
@@ -29,21 +30,18 @@ const PopularRecipe = () => {
                             <li>Salt and pepper to taste</li>
                             <li>Fresh parsley, chopped (for garnish)</li>
                         </ul>
+                        <br />
                     <p>Instructions</p>
                         <ol>
                             <li>
-                                Cook the pasta in a large pot of salted boiling water according to the package instructions until al dente.
-                                <br />Drain and set aside.
-                            </li>
+                                Cook the pasta in a large pot of salted boiling water according to the package instructions until al dente. Drain and set aside.
+                            </li><br />
                             <li>
-                                In a large skillet, heat the olive oil over medium-low heat. Add the sliced garlic and red pepper flakes.
-                                <br />Cook until the garlic becomes fragrant and just starts to turn golden (about 2-3 minutes).
-                                <br />Be careful not to burn the garlic.
-                            </li>
+                                In a large skillet, heat the olive oil over medium-low heat. Add the sliced garlic and red pepper flakes. Cook until the garlic becomes fragrant and just starts to turn golden (about 2-3 minutes). Be careful not to burn the garlic.
+                            </li><br />
                             <li>
-                                Add the cooked pasta to the skillet and toss it in the garlic and olive oil mixture.
-                                <br />Season with salt and pepper to taste.
-                            </li>
+                                Add the cooked pasta to the skillet and toss it in the garlic and olive oil mixture. Season with salt and pepper to taste.
+                            </li><br />
                             <li>Garnish with freshly chopped parsley and serve. You can also add a sprinkle of grated Parmesan cheese if desired.</li>
                         </ol>
                 </div>
@@ -51,6 +49,7 @@ const PopularRecipe = () => {
                     <h3 className='dish-name'>Nutty chicken satay strips</h3>
                     <img className='dish-img' src={chicken} alt='Chicken Satay'/>
                     <p>Cooking Time: 20 minutes</p>
+                    <br />
                     <p>Ingredients</p>
                         <ul>
                             <li>2 tbsp chunky peanut butter (without palm oil or sugar)</li>
@@ -62,23 +61,20 @@ const PopularRecipe = () => {
                             <li>about 10cm cucumber, cut into fingers</li>
                             <li>sweet chilli sauce, to serve</li>
                         </ul>
+                        <br />
                     <p>Instructions</p>
                         <ol>
                             <li>
                                 Heat oven to 200C/180C fan/gas 4 and line a baking tray with non-stick paper.
-                            </li>
+                            </li><br />
                             <li>
-                                Mix 2 tbsp chunky peanut butter with 1 finely grated garlic clove, 1 tsp Madras curry powder, a few shakes of soy sauce
-                                <br />and 2 tsp lime juice in a bowl. Some nut butters are thicker than others, so if necessary, add a dash of boiling
-                                <br />water to get a coating consistency.
-                            </li>
+                                Mix 2 tbsp chunky peanut butter with 1 finely grated garlic clove, 1 tsp Madras curry powder, a few shakes of soy sauce and 2 tsp lime juice in a bowl. Some nut butters are thicker than others, so if necessary, add a dash of boiling water to get a coating consistency.
+                            </li><br />
                             <li>
-                                Add 2 skinless chicken breast fillets, cut into strips, and mix well. Arrange on the baking sheet, spaced apart, and bake in
-                                <br />the oven for 8-10 mins until cooked, but still juicy.
-                            </li>
+                                Add 2 skinless chicken breast fillets, cut into strips, and mix well. Arrange on the baking sheet, spaced apart, and bake in the oven for 8-10 mins until cooked, but still juicy.
+                            </li><br />
                             <li>
-                                Eat warm with roughly 10cm cucumber, cut into fingers, and sweet chilli sauce.
-                                <br />Alternatively, leave to cool and keep in the fridge for up to 2 days.
+                                Eat warm with roughly 10cm cucumber, cut into fingers, and sweet chilli sauce. Alternatively, leave to cool and keep in the fridge for up to 2 days.
                             </li>
                         </ol>
                 </div>
@@ -86,6 +82,7 @@ const PopularRecipe = () => {
                     <h3 className='dish-name'>Sticky sweet chilli ginger salmon</h3>
                     <img className='dish-img' src={salmon} alt='Salmon'/>
                     <p>Cooking Time: 20 minutes</p>
+                    <br />
                     <p>Ingredients</p>
                         <ul>
                             <li>125ml (1/2 cup) sweet chilli sauce</li>
@@ -102,28 +99,23 @@ const PopularRecipe = () => {
                             <li>1 carrot, julienned into long strips</li>
                             <li>1 cup fresh mint leaves, plus extra 2 tbsp, to serve</li>
                         </ul>
+                        <br />
                     <p>Instructions</p>
                     <ol>
                         <li>
-                        Preheat the grill on high. Combine the sweet chilli sauce, soy sauce and ginger in a large bowl.
-                        <br />Add the salmon and gently mix until evenly coated. Set aside to marinate while you make the coleslaw.
-                        </li>
+                            Preheat the grill on high. Combine the sweet chilli sauce, soy sauce and ginger in a large bowl. Add the salmon and gently mix until evenly coated. Set aside to marinate while you make the coleslaw.
+                        </li><br />
                         <li>
-                            For the honey sesame coleslaw, whisk the vinegar, honey, sesame oil and salt in a small bowl.
-                            <br />Place the cabbage, carrot and mint in a large bowl.
-                        </li>
+                            For the honey sesame coleslaw, whisk the vinegar, honey, sesame oil and salt in a small bowl. Place the cabbage, carrot and mint in a large bowl.
+                        </li><br />
                         <li>
-                            Line a baking tray with foil and lightly brush or spray with oil. Remove salmon from bowl and shake off excess marinade,
-                            <br />reserving for later. Place the salmon on the prepared tray.
-                            <br />Cook under the grill, without turning, for 8-10 minutes or until just starting to char at edges and flake when tested with a fork.
-                        </li>
+                            Line a baking tray with foil and lightly brush or spray with oil. Remove salmon from bowl and shake off excess marinade, reserving for later. Place the salmon on the prepared tray. Cook under the grill, without turning, for 8-10 minutes or until just starting to char at edges and flake when tested with a fork.
+                        </li><br />
                         <li>
-                            Meanwhile, place the reserved marinade in a small saucepan over medium heat.
-                            <br />Bring to a simmer, then reduce heat to low and cook, stirring occasionally, for 3-4 minutes.
-                        </li>
+                            Meanwhile, place the reserved marinade in a small saucepan over medium heat. Bring to a simmer, then reduce heat to low and cook, stirring occasionally, for 3-4 minutes.
+                        </li><br />
                         <li>
-                            Lightly whisk the dressing again and pour over the cabbage mixture. Toss until well combined.
-                            <br />Divide the salmon among serving plates, drizzle over the reduced marinade and serve with the coleslaw and extra mint.
+                            Lightly whisk the dressing again and pour over the cabbage mixture. Toss until well combined. Divide the salmon among serving plates, drizzle over the reduced marinade and serve with the coleslaw and extra mint.
                         </li>
                     </ol>
                 </div>
