ce4244bb6904a975fe6cde8620e3cca2b9da36cd
Alexander Wilson <apwilson97@gmail.com>
2023-10-26 17:46:25 +0100

    Tidied up code by deleting old acebook files

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -5,7 +5,6 @@ import Generator from '../generator/Generator'
 import PopularRecipes from '../popular_recipes/PopularRecipes'
 import Home from '../Home/home';
 import React, { useState } from 'react';
-import Feed from '../feed/Feed'
 import {
   useNavigate,
   Routes,
@@ -18,7 +17,6 @@ import RecipeView from '../recipe_view/recipeView';
 const App = () => {
     return (
         <Routes>
-          <Route path='/recipe'  element={<Feed navigate={ useNavigate() }/>}/>
           <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
           <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
           <Route path='/generator' element={<Generator navigate={ useNavigate() }/>}/>
